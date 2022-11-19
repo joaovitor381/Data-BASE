66b45b2f9bf8188271a7016ecb4fb8c320dc8104
João vitor <joaoviitor888@outlook.com>
2022-11-19 18:12:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return <h1>olá mundo!</h1>;
+  return (
+    <>
+      <h1>olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
